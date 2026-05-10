8da100d6eec206e6bc6b5b6897a31e4da80ec46f
Saber <djsoumyajitdatta@gmail.com>
2026-05-10 12:48:48 +0530

    Add files via upload

---
+++ FIRMWARE/spoofer.h
@@ -0,0 +1,15 @@
+
+#ifndef spoofer_H
+#define spoofer_H
+
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
+#include <U8g2lib.h>
+#include <Adafruit_NeoPixel.h>
+#include "neopixel.h"
+
+void spooferSetup();
+void spooferLoop();
+
+#endif
